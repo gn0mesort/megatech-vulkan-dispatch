c2e7825a593e880cef9194d903dab28f4c1cc328
Alexander Rothman <gnomesort@megate.ch>
2024-08-14 13:51:53 -0700

    Add to_command Functions
    This commit adds functions for mapping hash values to command indices
    without retrieving any functions from a table. Potentially, this
    provides a large speed-up for clients that need to compute hashes at
    run-time. As long as they compute the hash and resolve the command index
    ahead of resolving the function pointer.

--- include/megatech/vulkan/dispatch/commands.hpp
+++ include/megatech/vulkan/dispatch/commands.hpp
@@ -26,6 +26,40 @@ namespace global {
   };
 #undef MEGATECH_VULKAN_DISPATCH_COMMAND
 
+/// @cond
+#define MEGATECH_VULKAN_DISPATCH_COMMAND(name) case dispatch::internal::base::fnv1a_cstr(#name): return command::name;
+/// @endcond
+  /**
+   * @brief Convert an FNV1a hash value into a Vulkan @ref command.
+   * @details This function is equivalent, largely to the @ref ::table::get(const std::size_t) const method.
+   *          The primary exception is that this function can throw a @ref dispatch::error.
+   *
+   *          The primary utility of this function is to store @ref command values for hashes computed at run-time.
+   *          Generally, storing a @ref command and retrieving the corresponding function pointer is much faster than
+   *          computing hash values multiple times from strings.
+   *
+   *          In a pinch, you could also use this to pre-resolve all of your hash values to @ref command values at
+   *          run-time. This would allow you to skip checking for null returns on
+   *          @ref ::table::get(const std::size_t) const.
+   * @param hash An FNV1a hash value that maps to a Vulkan @ref command. Hash values are mapped based on the string
+   *             value of the command's name. For example:
+   *
+   *             @code{.cpp}
+   *               to_command(fnv1a("vkGetInstanceProcAddr")); // == command::vkGetInstanceProcAddr
+   *             @endcode
+   *
+   * @throws dispatch::error If the input hash doesn't have a compile-time mapping.
+   */
+  constexpr command to_command(const std::size_t hash) {
+    switch (hash)
+    {
+    MEGATECH_VULKAN_DISPATCH_GLOBAL_COMMAND_LIST
+    default:
+      throw error{ "The input hash value does not map to any global Vulkan command." };
+    }
+  }
+#undef MEGATECH_VULKAN_DISPATCH_COMMAND
+
 }
 
 namespace instance {
@@ -41,6 +75,26 @@ namespace instance {
   };
 #undef MEGATECH_VULKAN_DISPATCH_COMMAND
 
+/// @cond
+#define MEGATECH_VULKAN_DISPATCH_COMMAND(name) case dispatch::internal::base::fnv1a_cstr(#name): return command::name;
+/// @endcond
+  /**
+   * @brief Convert an FNV1a hash value into a Vulkan command.
+   * @param hash An FNV1a hash value that maps to a Vulkan @ref command. Hash values are mapped based on the string
+   *             value of the command's name.
+   * @throws dispatch::error If the input hash doesn't have a compile-time mapping.
+   * @see ::global::to_command
+   */
+  constexpr command to_command(const std::size_t hash) {
+    switch (hash)
+    {
+    MEGATECH_VULKAN_DISPATCH_INSTANCE_COMMAND_LIST
+    default:
+      throw error{ "The input hash value does not map to any instance-level Vulkan command." };
+    }
+  }
+#undef MEGATECH_VULKAN_DISPATCH_COMMAND
+
 }
 
 namespace device {
@@ -54,6 +108,26 @@ namespace device {
   enum class command : std::size_t {
     MEGATECH_VULKAN_DISPATCH_DEVICE_COMMAND_LIST
   };
+#undef MEGATECH_VULKAN_DISPATCH_COMMAND
+
+/// @cond
+#define MEGATECH_VULKAN_DISPATCH_COMMAND(name) case dispatch::internal::base::fnv1a_cstr(#name): return command::name;
+/// @endcond
+  /**
+   * @brief Convert an FNV1a hash value into a Vulkan command.
+   * @param hash An FNV1a hash value that maps to a Vulkan @ref command. Hash values are mapped based on the string
+   *             value of the command's name.
+   * @throws dispatch::error If the input hash doesn't have a compile-time mapping.
+   * @see ::global::to_command
+   */
+  constexpr command to_command(const std::size_t hash) {
+    switch (hash)
+    {
+    MEGATECH_VULKAN_DISPATCH_DEVICE_COMMAND_LIST
+    default:
+      throw error{ "The input hash value does not map to any device-level Vulkan command." };
+    }
+  }
 #undef MEGATECH_VULKAN_DISPATCH_COMMAND
 
 }
